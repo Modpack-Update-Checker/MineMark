2436023f3e72bf45b9ad13034b9d8dcbd2343141
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-09 22:57:45 +1000

    oops

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,74 +1,74 @@
-package com.example.examplemod;
-
-//? if fabric
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.Minecraft;
-//? if forge {
-/*import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.common.Mod;
-*///?} if neoforge {
-/*import net.neoforged.fml.ModList;
-import net.neoforged.fml.common.Mod;
-*///?}
-//? if >=1.21 {
-/*import net.minecraft.client.gui.screens.options.OptionsScreen;
-*///?} else
-import net.minecraft.client.gui.screens.OptionsScreen;
-//? if >=1.18 {
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-//?} else {
-/*import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-*///?}
-import java.util.NoSuchElementException;
-
-//? if forge || neoforge
-/*@Mod(ExampleMod.MOD_ID)*/
-public class ExampleMod {
-    public static final String MOD_ID = "mpuc_minemark";
-	//? if >=1.18
-    public static final Logger LOGGER = LoggerFactory.getLogger(getDisplayName(MOD_ID));
-	//? if <1.18
-	/*public static final Logger LOGGER = LogManager.getLogger(getDisplayName(MOD_ID));*/
-
-    private static String getDisplayName(String modId) {
-        //? if fabric
-        return FabricLoader.getInstance().getModContainer(modId).orElseThrow(() -> new NoSuchElementException("No value present")).getMetadata().getName();
-        //? if forge
-        /*return ModList.get().getModContainerById(modId).orElseThrow(() -> new NoSuchElementException("No value present")).getModInfo().getDisplayName();*/
-        //? if neoforge
-        /*return ModList.get().getModContainerById(modId).orElseThrow(() -> new NoSuchElementException("No value present")).getModInfo().getDisplayName();*/
-    }
-
-    //? if forge || neoforge {
-    /*public ExampleMod() {
-        onInitialize();
-    }
-    *///?}
-
-    public void onInitialize() {
-        //? if fabric
-        String loader = "Fabric";
-        //? if forge
-        /*String loader = "Forge";*/
-        //? if neoforge
-        /*String loader = "NeoForge";*/
-        LOGGER.info("Hello {} World!", loader);
-        // This is a test to assert that the access widener works.
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while(true) {
-					synchronized (Minecraft.getInstance()) {
-						if (Minecraft.getInstance().screen instanceof OptionsScreen) {
-							Minecraft.getInstance().execute(() ->
-							Minecraft.getInstance().setScreen(new MarkdownTestGui()));
-							return;
-						}
-					}
-				}
-			}
-		}).start();
-    }
-}
+//package com.example.examplemod;
+//
+////? if fabric
+//import net.fabricmc.loader.api.FabricLoader;
+//import net.minecraft.client.Minecraft;
+////? if forge {
+///*import net.minecraftforge.fml.ModList;
+//import net.minecraftforge.fml.common.Mod;
+//*///?} if neoforge {
+///*import net.neoforged.fml.ModList;
+//import net.neoforged.fml.common.Mod;
+//*///?}
+////? if >=1.21 {
+///*import net.minecraft.client.gui.screens.options.OptionsScreen;
+//*///?} else
+//import net.minecraft.client.gui.screens.OptionsScreen;
+////? if >=1.18 {
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+////?} else {
+///*import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//*///?}
+//import java.util.NoSuchElementException;
+//
+////? if forge || neoforge
+///*@Mod(ExampleMod.MOD_ID)*/
+//public class ExampleMod {
+//    public static final String MOD_ID = "mpuc_minemark";
+//	//? if >=1.18
+//    public static final Logger LOGGER = LoggerFactory.getLogger(getDisplayName(MOD_ID));
+//	//? if <1.18
+//	/*public static final Logger LOGGER = LogManager.getLogger(getDisplayName(MOD_ID));*/
+//
+//    private static String getDisplayName(String modId) {
+//        //? if fabric
+//        return FabricLoader.getInstance().getModContainer(modId).orElseThrow(() -> new NoSuchElementException("No value present")).getMetadata().getName();
+//        //? if forge
+//        /*return ModList.get().getModContainerById(modId).orElseThrow(() -> new NoSuchElementException("No value present")).getModInfo().getDisplayName();*/
+//        //? if neoforge
+//        /*return ModList.get().getModContainerById(modId).orElseThrow(() -> new NoSuchElementException("No value present")).getModInfo().getDisplayName();*/
+//    }
+//
+//    //? if forge || neoforge {
+//    /*public ExampleMod() {
+//        onInitialize();
+//    }
+//    *///?}
+//
+//    public void onInitialize() {
+//        //? if fabric
+//        String loader = "Fabric";
+//        //? if forge
+//        /*String loader = "Forge";*/
+//        //? if neoforge
+//        /*String loader = "NeoForge";*/
+//        LOGGER.info("Hello {} World!", loader);
+//        // This is a test to assert that the access widener works.
+//		new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				while(true) {
+//					synchronized (Minecraft.getInstance()) {
+//						if (Minecraft.getInstance().screen instanceof OptionsScreen) {
+//							Minecraft.getInstance().execute(() ->
+//							Minecraft.getInstance().setScreen(new MarkdownTestGui()));
+//							return;
+//						}
+//					}
+//				}
+//			}
+//		}).start();
+//    }
+//}
--- src/main/java/com/example/examplemod/MarkdownTestGui.java
+++ src/main/java/com/example/examplemod/MarkdownTestGui.java
@@ -1,87 +1,87 @@
-/*
- * This file is part of MineMark
- * Copyright (C) 2024 DeDiamondPro
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License Version 3 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
-package com.example.examplemod;
-
-import dev.jab125.minemark.minecraft.MineMarkDrawable;
-//? if >=1.20
-import net.minecraft.client.gui.GuiGraphics;
-//? if <1.20
-/*import com.mojang.blaze3d.vertex.PoseStack;*/
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
-//? if <1.19
-/*import net.minecraft.network.chat.TextComponent;*/
-import org.xml.sax.SAXException;
-
-
-import java.io.IOException;
-
-public class MarkdownTestGui extends Screen {
-    private final MineMarkDrawable markdown;
-
-    protected MarkdownTestGui() {
-        super(/*? if >=1.19 {*/ Component.literal/*?} else {*/ /*new TextComponent*//*?}*/("Markdown Test GUI"));
-        try {
-            String markdownString = "Test **string** *with* a<br>newline <u>because</u> ~~why~~ not and also a [link](https://example.com), this should also automatically wrap if I make this text long enough<br>"
-                    + "Image: ![](https://picsum.photos/2000/1000)"
-                    + "\n# Heading 1"
-                    + "\n## Heading 2"
-                    + "\n### Heading 3"
-                    + "\n#### Heading 4"
-                    + "\n##### Heading 5"
-                    + "\n---"
-                    + "\nThis `is an inline` codeblock!"
-                    + "\n```\nAnd this one isn't inline\nand has multiple lines!\n```"
-                    + "\n> And this is a blockquote<br>because why not?"
-                    + "\n1. And an ordered list"
-                    + "\n   - With an unordered list inside"
-                    + "\n   - like this"
-                    + "\n2. :)"
-                    + "\n\n| Hi | Test | E       | E | E |\n" +
-                    "|----|------|---------|---|---|\n" +
-                    "| E  | E    | E       | E |   |\n" +
-                    "| E  | E    | EEEEEEE |   | E |\n" +
-                    "| E  | E    | E       | E |   |";
-            markdown = new MineMarkDrawable(markdownString);
-            // System.out.println(markdown.getParsedMarkdown().getTree());
-        } catch (IOException | SAXException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void render(/*? if >=1.20 {*/GuiGraphics/*?} else {*//*PoseStack*//*?}*/ graphics, int mouseX, int mouseY, float delta) {
-        //? >=1.20 {
-        super.render(graphics, mouseX, mouseY, delta);
-        //?} else
-        /*super.renderBackground(graphics);*/
-        markdown.draw(20, 20, 300, mouseX, mouseY, graphics);
-    }
-
-    @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        markdown.onMouseClicked(20, 20, (float) mouseX, (float) mouseY, button);
-        return super.mouseClicked(mouseX, mouseY, button);
-    }
-
-    @Override
-    public void onClose() {
-        markdown.close();
-        super.onClose();
-    }
-}
+///*
+// * This file is part of MineMark
+// * Copyright (C) 2024 DeDiamondPro
+// *
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU Lesser General Public
+// * License Version 3 as published by the Free Software Foundation.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// * Lesser General Public License for more details.
+// *
+// * You should have received a copy of the GNU Lesser General Public License
+// * along with this program. If not, see <https://www.gnu.org/licenses/>.
+// */
+//
+//package com.example.examplemod;
+//
+//import dev.jab125.minemark.minecraft.MineMarkDrawable;
+////? if >=1.20
+//import net.minecraft.client.gui.GuiGraphics;
+////? if <1.20
+///*import com.mojang.blaze3d.vertex.PoseStack;*/
+//import net.minecraft.client.gui.screens.Screen;
+//import net.minecraft.network.chat.Component;
+////? if <1.19
+///*import net.minecraft.network.chat.TextComponent;*/
+//import org.xml.sax.SAXException;
+//
+//
+//import java.io.IOException;
+//
+//public class MarkdownTestGui extends Screen {
+//    private final MineMarkDrawable markdown;
+//
+//    protected MarkdownTestGui() {
+//        super(/*? if >=1.19 {*/ Component.literal/*?} else {*/ /*new TextComponent*//*?}*/("Markdown Test GUI"));
+//        try {
+//            String markdownString = "Test **string** *with* a<br>newline <u>because</u> ~~why~~ not and also a [link](https://example.com), this should also automatically wrap if I make this text long enough<br>"
+//                    + "Image: ![](https://picsum.photos/2000/1000)"
+//                    + "\n# Heading 1"
+//                    + "\n## Heading 2"
+//                    + "\n### Heading 3"
+//                    + "\n#### Heading 4"
+//                    + "\n##### Heading 5"
+//                    + "\n---"
+//                    + "\nThis `is an inline` codeblock!"
+//                    + "\n```\nAnd this one isn't inline\nand has multiple lines!\n```"
+//                    + "\n> And this is a blockquote<br>because why not?"
+//                    + "\n1. And an ordered list"
+//                    + "\n   - With an unordered list inside"
+//                    + "\n   - like this"
+//                    + "\n2. :)"
+//                    + "\n\n| Hi | Test | E       | E | E |\n" +
+//                    "|----|------|---------|---|---|\n" +
+//                    "| E  | E    | E       | E |   |\n" +
+//                    "| E  | E    | EEEEEEE |   | E |\n" +
+//                    "| E  | E    | E       | E |   |";
+//            markdown = new MineMarkDrawable(markdownString);
+//            // System.out.println(markdown.getParsedMarkdown().getTree());
+//        } catch (IOException | SAXException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @Override
+//    public void render(/*? if >=1.20 {*/GuiGraphics/*?} else {*//*PoseStack*//*?}*/ graphics, int mouseX, int mouseY, float delta) {
+//        //? >=1.20 {
+//        super.render(graphics, mouseX, mouseY, delta);
+//        //?} else
+//        /*super.renderBackground(graphics);*/
+//        markdown.draw(20, 20, 300, mouseX, mouseY, graphics);
+//    }
+//
+//    @Override
+//    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+//        markdown.onMouseClicked(20, 20, (float) mouseX, (float) mouseY, button);
+//        return super.mouseClicked(mouseX, mouseY, button);
+//    }
+//
+//    @Override
+//    public void onClose() {
+//        markdown.close();
+//        super.onClose();
+//    }
+//}
